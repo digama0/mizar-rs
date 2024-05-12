29a86d6329c267a89738a9832b120703002a3c45
Mario Carneiro <di.gama@gmail.com>
2024-05-12 17:34:39 +0200

    bump MML v. 8.1.14-5.78.1462

--- download-mml.sh
+++ download-mml.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p miz/zip
 cd miz/zip
-wget http://mizar.uwb.edu.pl/~softadm/pub/system/i386-win32/mizar-8.1.12_5.74.1441-i386-win32.exe -O mizar.exe
+wget https://mizar.uwb.edu.pl/~softadm/pub/system/i386-win32/mizar-8.1.14_5.78.1462-i386-win32.exe -O mizar.exe
 unzip mizar.exe
 mkdir -p ../mizshare/mml ../mizshare/prel
 cd ../mizshare/mml
